e7f067a9a541b21a2829acf6468c55cc596908fd
tylerjyee <tyler.j.yee@sjsu.edu>
2023-04-19 10:36:16 -0700

    updates to routes and html files

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,49 @@
 from flask import render_template
 from flask import redirect
 from flask import flash
+from flask import url_for
 from .forms import LoginForm
 from app import myapp_obj
 from flask_login import current_user
 from flask_login import login_user
 from flask_login import logout_user
 from flask_login import login_required
-from flask_mail import mail
 
+@myapp_obj.route("/", methods=["GET"])
+def to_home():
+    return redirect(url_for('home'))
+
+@myapp_obj.route("/home")
+def home():
+    return render_template('home.html', title = "home")
+
+@myapp_obj.route("/login", methods=['GET', 'POST'])
+def login():
+    # create form
+    form = LoginForm()
+    # if form inputs are valid
+    if form.validate_on_submit():
+        # search database for username
+        # user = User.query.filter_by(...)
+        # check the password
+        # if password matches
+        # login_user(user)
+        flash(f'Here are the input {form.username.data} and {form.password.data}')
+        return redirect('/')
+    return render_template('login.html', form=form)
+
+@myapp_obj.route("/register")
+def register():
+    return render_template('register.html')
+
+@myapp_obj.route("/todo")
+def todo():
+    return render_template('todo.html',)
+
+@myapp_obj.route("/emails")
+def emails():
+    return render_template('email.html')
+"""
 @myapp_obj.route("/")
 @myapp_obj.route("/index.html")
 def index():
@@ -44,7 +79,7 @@ def getMember(name):
     return escape(name)
 
 
-"""@myapp_obj.route("/compose")
+@myapp_obj.route("/compose")
 @login_required
 def compose():
     return render_template('compose.html')"""
