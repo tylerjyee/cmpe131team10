c5da93176cf1b8d3bc98036c9e2624ed832bcbf3
phillipgeezus <phillipomgee@gmail.com>
2023-04-28 23:15:56 -0700

    updated chat

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
-
+db.create_all()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True)
